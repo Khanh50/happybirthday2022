261930b4857e5cdc302515b017927821de3aae17
Khanh50 <70705683+Khanh50@users.noreply.github.com>
2022-09-26 15:30:13 +0700

    upload all files

---
+++ index.js
@@ -0,0 +1,59 @@
+// const url = "https://api.quotable.io/random";
+const url = "https://free-quotes-api.herokuapp.com/";
+
+generateQuote();
+
+document.querySelector("button").addEventListener("click", generateQuote);
+
+
+function generateQuote(){
+   fetch(url)
+   .then(function(data) {
+      return data.json();
+   })
+   .then(function(data){    
+      document.querySelector("blockquote p").innerHTML = data.quote;
+      var author = data.author;
+      if (author == ""){
+         author = "Someone";
+      }
+      document.querySelector("figcaption").innerHTML = author;
+   })
+   .catch(function(err) {
+      console.log(err); 
+   });
+}
+
+let options = {
+   root: document.querySelector('#scrollArea'),
+   rootMargin: '0px',
+   threshold: 0.005,
+}
+ 
+let observer = new IntersectionObserver((elements) => {
+   elements.forEach((e) => {
+      if (e.isIntersecting){
+         e.target.classList.add("fade-in");
+         setTimeout(() => {
+            e.target.style.opacity = "0.9";
+            e.target.classList.remove("fade-in");
+         }, 1200);         
+
+      }            
+      if (e.intersectionRect.top == 0){         
+         e.target.style.opacity = "0";
+      }
+   })
+}, options);
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const handleScrollAnimation = () => {
+   scrollElements.forEach((element) =>{
+      observer.observe(element);
+   })
+}
+
+window.addEventListener('scroll', () => {
+   handleScrollAnimation();
+})
